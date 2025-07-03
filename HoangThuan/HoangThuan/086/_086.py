50a5186784b251783edd212598c256b14bc7ec80
NightMareScorn <NightMareScorn@gmail.com>
2025-07-03 22:41:56 +0700

    B086

---
+++ HoangThuan/HoangThuan/086/_086.py
@@ -0,0 +1,16 @@
+
+x = int(input("Nhap x: "))
+n = int(input("Nhap n: "))
+
+s = 0
+t = 1
+i = 2
+dau = -1
+
+while(i <= 2 * n):
+        t = t * x * x
+        s = s + dau * t
+        i = i + 2
+        dau = -dau
+
+print(s)
