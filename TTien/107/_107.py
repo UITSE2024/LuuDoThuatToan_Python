0d4b408baa4372db185492b1507b994c89c11f8f
Thao-Tien <lilianjung2036@gmail.com>
2025-07-04 12:23:09 +0700

    107

---
+++ TTien/107/_107.py
@@ -0,0 +1,20 @@
+
+import math
+
+x = float(input("Nhap x: "))
+
+xx = (x*math.pi)/180
+s = 1
+t = 1
+m = 1
+dau = -1
+e = 1
+i = 2
+while(e>= 10**-6):
+    t = t * xx * xx
+    m = m * (i-1) * i
+    e = t/m
+    s = s + dau*e
+    dau = -dau
+    i = i+2
+print(s)
